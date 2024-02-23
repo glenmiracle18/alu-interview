af4c69ff1e797fa9c59e7fd407f048fbdb1dc9fd
glenmiracle18 <m.bonyu@alustudent.com>
2024-02-23 21:31:13 +0200

    done: fully working pascal's triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
 """
-funciton for pascals triangle
+pascal's triangle
 """
 
 
 def pascal_triangle(n):
     """
-    Generates Pascal's triangle up to n rows using an iterative approach.
+    Generates pascal's triangle (inteeractive approach)
     Args:
-      n: The number of rows in the triangle.
+        n: the number of rows in the triangle
     Returns:
-      A list of lists representing the Pascal's triangle.
+        A list of list representing the P triangle
     """
     triangle = []
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
-        row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
         triangle.append(row)
     return triangle
